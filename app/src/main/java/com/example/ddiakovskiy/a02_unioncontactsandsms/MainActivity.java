9228dc8e18a17fe5fd8f5626ff2e06e02711df9b
Diakovskiy <ganwhite@gmail.com>
2018-12-04 17:53:57 +0200

    first working release - contacts almost ok sms - not implemented

--- app/src/main/java/com/example/ddiakovskiy/a02_unioncontactsandsms/MainActivity.java
+++ app/src/main/java/com/example/ddiakovskiy/a02_unioncontactsandsms/MainActivity.java
@@ -30,20 +30,14 @@ public class MainActivity extends Activity {
     }
 
     public void onExportContactsAndSMS(View v){
+
         Log.d(TAG, "onExportContacts: exporting");
 
-        // Check the SDK version and whether the permission is already granted or not.
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
             requestPermissions(new String[]{Manifest.permission.READ_CONTACTS}, PERMISSIONS_REQUEST_READ_CONTACTS);
-            //After this point you wait for callback in onRequestPermissionsResult(int, String[], int[]) overriden method
         } else {
             reallyExportContactsAndSMS();
         }
-    }
-
-    public void onImportContacts(View v){
-        reallyImportContacts();
-
     }
 
     @Override
@@ -59,76 +53,22 @@ public class MainActivity extends Activity {
         }
     }
 
+    public void onImportContacts(View v){
+        reallyImportContacts();
+    }
 
     public void reallyExportContactsAndSMS(){
 
-        SimplePhoneNumber.getSimplePhoneNumbers(this);
-        Toast.makeText(this, "Export sucess", Toast.LENGTH_LONG).show();
+        PhoneBookUtil.ExportPhoneNumbersToFile(this);
 
     }
-
 
     public void reallyImportContacts(){
 
-        String name = "MY_DDD1";
+        PhoneBookUtil.ImportPhoneNumbersFromFile(this);
 
-        String phone = "0666285092";
-
-        ArrayList<ContentProviderOperation> op = new ArrayList<ContentProviderOperation>();
-
-        /* Добавляем пустой контакт */
-        op.add(ContentProviderOperation.newInsert(ContactsContract.RawContacts.CONTENT_URI)
-                .withValue(ContactsContract.RawContacts.ACCOUNT_TYPE, null)
-                .withValue(ContactsContract.RawContacts.ACCOUNT_NAME, null)
-                .build());
-        /* Добавляем данные имени */
-        op.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
-                .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, 0)
-                .withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE)
-                .withValue(ContactsContract.CommonDataKinds.StructuredName.DISPLAY_NAME, name)
-                .build());
-        /* Добавляем данные телефона */
-        op.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
-                .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, 0)
-                .withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE)
-                .withValue(ContactsContract.CommonDataKinds.Phone.NUMBER, phone)
-                .withValue(ContactsContract.CommonDataKinds.Phone.TYPE, ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE)
-                .build());
-
-        try {
-            getContentResolver().applyBatch(ContactsContract.AUTHORITY, op);
-        } catch (Exception e) {
-            Log.e("Exception: ", e.getMessage());
-        }
 
 
     }
-
-    public void reallyImportContacts2(){
-
-        String newContact = "MY_NEW_CONTACT";
-
-
-        ContentValues contactValues = new ContentValues();
-        contactValues.put(ContactsContract.RawContacts.ACCOUNT_NAME, newContact);
-        contactValues.put(ContactsContract.RawContacts.ACCOUNT_TYPE, newContact);
-        Uri newUri = getContentResolver().insert(ContactsContract.RawContacts.CONTENT_URI, contactValues);
-        long rawContactsId = ContentUris.parseId(newUri);
-
-        contactValues.clear();
-
-        contactValues.put(ContactsContract.Data.RAW_CONTACT_ID, rawContactsId);
-        contactValues.put(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE);
-
-        contactValues.put(ContactsContract.CommonDataKinds.StructuredName.DISPLAY_NAME, newContact);
-        getContentResolver().insert(ContactsContract.Data.CONTENT_URI, contactValues);
-
-        Toast.makeText(getApplicationContext(), newContact + " добавлен в список контактов", Toast.LENGTH_LONG).show();
-
-
-        //Toast.makeText(this, "Really import contacts", Toast.LENGTH_LONG).show();
-
-    }
-
 
 }
